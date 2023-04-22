6221cff2d17e799754fd332e7a0538f52b5c5e27
LinuxSBC <benedict.p.simmons@gmail.com>
2023-04-22 17:43:55 -0500

    Add extra config to build

--- build.sh
+++ build.sh
@@ -10,6 +10,14 @@ echo "---"
 
 # remove the default firefox (from fedora) in favor of the flatpak
 rpm-ostree override remove firefox firefox-langpacks
+# remove default GNOME Extensions app in favor of the Extension Manager flatpak
+rpm-ostree override remove gnome-extensions-app
+
+echo "-- Installing adw-gtk3 COPR repo --"
+curl https://copr.fedorainfracloud.org/coprs/nickavem/adw-gtk3/repo/fedora-${FEDORA_MAJOR_VERSION}/nickavem-adw-gtk3-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/nickavem-adw-gtk3-fedora-${FEDORA_MAJOR_VERSION}.repo
+
+echo "-- Installing MoreWaita COPR repo --"
+curl https://copr.fedorainfracloud.org/coprs/dusansimic/themes/repo/fedora-${FEDORA_MAJOR_VERSION}/dusansimic-themes-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/dusansimic-themes-fedora-${FEDORA_MAJOR_VERSION}.repo
 
 repos=$(yq '.extrarepos[]' < /usr/etc/ublue-recipe.yml)
 if [[ -n "$repos" ]]; then
@@ -27,6 +35,14 @@ for pkg in $(echo -e "$rpm_packages"); do \
     rpm-ostree install $pkg; \
 done
 echo "---"
+
+echo "-- Configuring Distrobox --"
+mkdir -p /etc/distrobox
+echo "container_image_default=\"registry.fedoraproject.org/fedora-toolbox:$(rpm -E %fedora)\"" >> /etc/distrobox/distrobox.conf
+
+echo "-- Setting themes --"
+gsettings set org.gnome.desktop.interface icon-theme 'MoreWaita'
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
 
 # install yafti to install flatpaks on first boot, https://github.com/ublue-os/yafti
 pip install --prefix=/usr yafti
