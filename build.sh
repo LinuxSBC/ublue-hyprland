15ff13747189869cebcac85c048a8c686b3d49dd
LinuxSBC <benedict.p.simmons@gmail.com>
2023-04-24 17:32:43 -0500

    Fix merge again

--- build.sh
+++ build.sh
@@ -11,12 +11,6 @@ echo "---"
 # remove the default firefox (from fedora) in favor of the flatpak
 rpm-ostree override remove firefox firefox-langpacks
 
-echo "-- Installing adw-gtk3 COPR repo --"
-curl https://copr.fedorainfracloud.org/coprs/nickavem/adw-gtk3/repo/fedora-${FEDORA_MAJOR_VERSION}/nickavem-adw-gtk3-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/nickavem-adw-gtk3-fedora-${FEDORA_MAJOR_VERSION}.repo
-
-echo "-- Installing MoreWaita COPR repo --"
-curl https://copr.fedorainfracloud.org/coprs/dusansimic/themes/repo/fedora-${FEDORA_MAJOR_VERSION}/dusansimic-themes-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/dusansimic-themes-fedora-${FEDORA_MAJOR_VERSION}.repo
-
 repos=$(yq '.extrarepos[]' < /usr/etc/ublue-recipe.yml)
 if [[ -n "$repos" ]]; then
     echo "-- Adding repos defined in recipe.yml --"
@@ -25,6 +19,18 @@ if [[ -n "$repos" ]]; then
     done
     echo "---"
 fi
+
+echo "-- Installing adw-gtk3 COPR repo --"
+curl https://copr.fedorainfracloud.org/coprs/nickavem/adw-gtk3/repo/fedora-${FEDORA_MAJOR_VERSION}/nickavem-adw-gtk3-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/nickavem-adw-gtk3-fedora-${FEDORA_MAJOR_VERSION}.repo
+
+echo "-- Installing MoreWaita COPR repo --"
+curl https://copr.fedorainfracloud.org/coprs/dusansimic/themes/repo/fedora-${FEDORA_MAJOR_VERSION}/dusansimic-themes-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/dusansimic-themes-fedora-${FEDORA_MAJOR_VERSION}.repo
+
+echo "-- Installing Hyprland COPR repo --"
+curl https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${FEDORA_MAJOR_VERSION}/solopasha-hyprland-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/solopasha-hyprland-fedora-${FEDORA_MAJOR_VERSION}.repo
+
+echo "-- Installing EWW COPR repo --"
+curl https://copr.fedorainfracloud.org/coprs/axeld/eww/repo/fedora-${FEDORA_MAJOR_VERSION}/axeld-eww-fedora-${FEDORA_MAJOR_VERSION}.repo > /etc/yum.repos.d/axeld-wee-fedora-${FEDORA_MAJOR_VERSION}.repo
 
 echo "-- Installing RPMs defined in recipe.yml --"
 rpm_packages=$(yq '.rpms[]' < /usr/etc/ublue-recipe.yml)
@@ -71,7 +77,7 @@ pip install --prefix=/usr yafti
 # add a package group for yafti using the packages defined in recipe.yml
 flatpaks=$(yq '.flatpaks[]' < /tmp/ublue-recipe.yml)
 # only try to create package group if some flatpaks are defined
-if [[ -n "$flatpaks" ]]; then            
+if [[ -n "$flatpaks" ]]; then
     yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /usr/etc/yafti.yml
     yq -i '.screens.applications.values.groups.Custom.default = true' /usr/etc/yafti.yml
     for pkg in $(echo -e "$flatpaks"); do \
